d03420095922e80161f4be5706dea2d4aeae6a2d
Omar Eldeeb <143250826+OmarEL-Deeb@users.noreply.github.com>
2026-03-06 14:16:40 +0200

    Add Go statements for SQL view separation
    Added 'Go' statements to separate SQL view definitions.

--- 04_Views.sql
+++ 04_Views.sql
@@ -6,6 +6,7 @@ AS
 select DriverId,FullName
 from Drivers
 where IsActive =1
+    Go
 --ShipmentSummary
 CREATE VIEW View_ShipmentSummary AS
 SELECT 
@@ -20,6 +21,7 @@ JOIN Customers c ON s.CustomerId = c.CustomerId
 LEFT JOIN Payments p ON s.ShipmentId = p.ShipmentId
 LEFT JOIN Vehicles v ON s.VehicleId = v.VehicleId
 LEFT JOIN Drivers d ON v.AssignedDriverId = d.DriverId;
+Go
 --MonthlyRevenue
 CREATE VIEW View_MonthlyRevenue AS
 SELECT 
@@ -29,6 +31,7 @@ SELECT
 FROM Payments
 WHERE IsPaid = 1
 GROUP BY YEAR(PaidDate), MONTH(PaidDate);
+Go
 --UnpaidShipments
 CREATE VIEW View_UnpaidShipments AS
 SELECT s.*
